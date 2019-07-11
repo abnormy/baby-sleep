5ac1048ec73adc0764adfb592eefb82186028d55
Alexey Rebrov <abnormy@mail.ru>
2019-07-11 16:45:54 +0300

    Some UI

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 //import React from "react"
 //import ReactDOM from "react-dom"
 
-ReactDOM.render(React.createElement(
-  "h1",
-  null,
-  "Hello ReactJS"
-), document.getElementById("root"));
+//ReactDOM.render(<h1>Hello ReactJS</h1>, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 //import React from "react"
 //import ReactDOM from "react-dom"
 
-ReactDOM.render(<h1>Hello ReactJS</h1>, document.getElementById("root"));
+//ReactDOM.render(<h1>Hello ReactJS</h1>, document.getElementById("root"));
